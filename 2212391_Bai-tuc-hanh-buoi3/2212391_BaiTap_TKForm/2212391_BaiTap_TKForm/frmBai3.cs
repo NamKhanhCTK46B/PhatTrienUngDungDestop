20e5481dd565cbc6abd362495faed2bf5566f744
NamKhanhCTK46B <2212391@dlu.edu.vn>
2024-08-24 23:43:56 +0700

    BaiThucHanhWindowFormBuoi3

---
+++ 2212391_Bai-tuc-hanh-buoi3/2212391_BaiTap_TKForm/2212391_BaiTap_TKForm/frmBai3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212391_BaiTap_TKForm
+{
+    public partial class frmBai3 : Form
+    {
+        List<String> nghiaTu = new List<String>();
+        public frmBai3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThemTu_Click(object sender, EventArgs e)
+        {
+            var tu = txtTuMoi.Text;
+            var nghia = txtNghia.Text;
+            listBox1.Items.Add(tu);
+            nghiaTu.Add(nghia);
+
+            txtTuMoi.Focus();
+            txtTuMoi.Text = "";
+            txtNghia.Text = "";
+
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            txtHienThiNghia.Text = nghia;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var stt = listBox1.SelectedIndex;
+            txtHienThiNghia.Text = nghiaTu[stt];
+        }
+    }
+}
---
+++ 2212391_Bai-tuc-hanh-buoi3/2212391_BaiTap_TKForm/2212391_BaiTap_TKForm/frmBai4.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace _2212391_BaiTap_TKForm
+{
+    partial class frmBai4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSo = new System.Windows.Forms.TextBox();
+            this.btnTimSo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(64, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(211, 244);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(366, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập số cần tìm";
+            // 
+            // txtSo
+            // 
+            this.txtSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSo.Location = new System.Drawing.Point(516, 111);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(158, 27);
+            this.txtSo.TabIndex = 2;
+            // 
+            // btnTimSo
+            // 
+            this.btnTimSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimSo.Location = new System.Drawing.Point(488, 174);
+            this.btnTimSo.Name = "btnTimSo";
+            this.btnTimSo.Size = new System.Drawing.Size(119, 36);
+            this.btnTimSo.TabIndex = 3;
+            this.btnTimSo.Text = "Tìm số";
+            this.btnTimSo.UseVisualStyleBackColor = true;
+            this.btnTimSo.Click += new System.EventHandler(this.btnTimSo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(366, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kết quả";
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.ForeColor = System.Drawing.Color.Red;
+            this.lblKetQua.Location = new System.Drawing.Point(494, 256);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(44, 25);
+            this.lblKetQua.TabIndex = 1;
+            this.lblKetQua.Text = "----";
+            // 
+            // frmBai4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 450);
+            this.Controls.Add(this.btnTimSo);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frmBai4";
+            this.Text = "frmBai4";
+            this.Load += new System.EventHandler(this.frmBai4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSo;
+        private System.Windows.Forms.Button btnTimSo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
